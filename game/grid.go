e4680beaf17474f73ea06f382799a1f63f196e55
Marc Haye <marc.antonio.haye@gmail.com>
2025-09-27 12:57:27 +0200

    refactor: optimize UpdateCellAdjs

--- game/grid.go
+++ game/grid.go
@@ -16,32 +16,38 @@ func NewGrid(len int) *Grid {
 	return &m
 }
 
-func (m Grid) InitiateCellAdjs(i, j int) {
+func (m Grid) UpdateCellAdjs(i, j int) {
 
 	adjs := make([]cell.Cell, 8)
-
-	adjs[0] = m.GetCell(i+1, j-1)
-	adjs[1] = m.GetCell(i-1, j)
-	adjs[2] = m.GetCell(i+1, j+1)
-	adjs[3] = m.GetCell(i, j-1)
-
-	adjs[4] = m.GetCell(i, j+1)
-	adjs[5] = m.GetCell(i-1, j-1)
-	adjs[6] = m.GetCell(i+1, j)
-	adjs[7] = m.GetCell(i-1, j+1)
-
 	cell := m.GetCell(i, j)
-	cell.SetAdjacents(adjs)
+
+	// For each possible adjacent position
+	for _, position := range GetAdjacentsPos() {
+		// Verify grid border case (to avoid Ci = -1)
+		if m.IsValidPosition(i+position[0], j+position[1]) {
+			// Appends each adjacent alive cell
+			if adjCell := m.GetCell(i+position[0], j+position[1]); adjCell.IsAlive {
+				adjs = append(adjs, *adjCell)
+			}
+
+		}
+	}
+	cell.SetAdjacent(adjs)
 }
 
 func (m Grid) UpdateCells() {
 	for i := range m {
 		for j := range m[i] {
-			c1 := m.GetCell(i, j)
-			if c1.IsAlive() {
-				for _, c2 := range c1.GetAdjacents() {
-					c2.UpdateState()
+			// For each alive cell
+			c := m.GetCell(i, j)
+			if c.IsAlive {
+				m.UpdateCellAdjs(i, j)
+				c.UpdateState()
+				for _, cellAdj := range c.Adjacents {
+					// Update cell and its adjacents
+					cellAdj.UpdateState()
 				}
+
 			}
 		}
 	}
@@ -50,12 +56,24 @@ func (m Grid) UpdateCells() {
 func (m Grid) Show() {
 	for i := range m {
 		for j := range m[i] {
-			fmt.Print(m[i][j], " ")
+			fmt.Print(m.GetCell(i, j).ToString(), " ")
 		}
 		fmt.Println(" ")
 	}
 }
 
-func (m Grid) GetCell(i, j int) cell.Cell {
-	return m[i][j]
+func (m Grid) GetCell(i, j int) *cell.Cell {
+	return &m[i][j]
+}
+
+// All possible adjacent cell position
+func GetAdjacentsPos() [8][2]int {
+	return [8][2]int{
+		{-1, -1}, {-1, 0}, {-1, 1}, // upper level
+		{0, -1}, {0, 1}, // left and right level
+		{1, -1}, {1, 0}, {1, 1}} // bottom level
+}
+
+func (m Grid) IsValidPosition(i, j int) bool {
+	return i >= 0 && i < len(m) && j >= 0 && j < len(m[0])
 }
